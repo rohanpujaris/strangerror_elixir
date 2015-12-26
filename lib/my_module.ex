c6061ff08f513c554d81cae7a9a273137592e6b7
Rohan Pujari <rohan.pujari@kiprosh.com>
2015-12-27 00:58:21 +0530

    Reduced code to focus on problamatic code

--- lib/my_module.ex
+++ lib/my_module.ex
@@ -1,12 +1,10 @@
 defmodule My.MyModule do
-  defmodule My.CustomBehaviour do
-    @callback function(String.t) :: String.t
+  defmodule My.CustomModule do
   end
 
   defmacro __using__(_) do
     quote do
       import My.MyModule
-      @behaviour My.CustomBehaviour
     end
   end
 end
